95876375cafa09a109820a6848c1aa872d2462e3
Matt Carr <matt@smartcaresystem.com>
2015-11-10 03:18:52 -0600

    Added timestamp to Messages Create Table statement

--- Database/SQLMessages.sql
+++ Database/SQLMessages.sql
@@ -2,14 +2,12 @@ USE test;
 
 CREATE TABLE Messages
 (
-	id int UNSIGNED NOT NULL AUTO_INCREMENT,
-	sender varchar (16) NOT NULL,
-	receiver varchar(16) NOT NULL,
-	`subject` varchar(256),
-	content varchar(512) NOT NULL,
-	PRIMARY KEY (id)
+    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `sender` varchar (16) NOT NULL,
+    `receiver` varchar(16) NOT NULL,
+    `subject` varchar(256),
+    `content` varchar(512) NOT NULL,
+    `type` varchar(16) NOT NULL,
+    `timestamp` DATETIME,
+    PRIMARY KEY (id)
 );
-
-INSERT INTO Messages(sender, receiver, `subject`, content)
-SELECT 'matthew', 'FatalCatharsis', 'test message', 'This is a test message in the messages table'
-UNION ALL SELECT 'JTuggle', 'matthew', 'test message2', 'This is a second test message in the messages table';
