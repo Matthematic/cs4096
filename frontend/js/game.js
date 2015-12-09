f6ab0dfd335e2823601109547eed07ade6047d9f
Wyatt Kennedy <wykennedy@gmail.com>
2015-12-09 15:40:11 -0600

    less sleepiness

--- frontend/js/game.js
+++ frontend/js/game.js
@@ -364,8 +364,22 @@
                 }
             }
         }
-        var d = expiryDate - Date();
-        $('#timeRemaining').text(expiryDate.getMinutes() + ":" + expiryDate.getSeconds());
+        function getTimeRemaining(endtime){
+            var t = Date.parse(endtime) - Date.parse(new Date());
+            var seconds = Math.floor( (t/1000) % 60 );
+            var minutes = Math.floor( (t/1000/60) % 60 );
+            var hours = Math.floor( (t/(1000*60*60)) % 24 );
+            var days = Math.floor( t/(1000*60*60*24) );
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        };
+        var obj = getTimeRemaining(expiryDate);
+        $('#timeRemaining').text(obj.minutes + ":" + obj.seconds;
         window.requestAnimationFrame(draw);
     };
     window.requestAnimationFrame(draw);
